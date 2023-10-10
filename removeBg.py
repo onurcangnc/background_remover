7850b666d547e84b741bc7ef2a2edd29bdf4f68d
Onurcan Genç <onurcan.genc@ug.bilkent.edu.tr>
2023-10-10 23:59:35 +0300

    Verbosity options added.

--- removeBg.py
+++ removeBg.py
@@ -6,5 +6,9 @@ output_path = './images/removedBackgroundme.jpeg'
 with open(input_path, 'rb') as i:
     with open(output_path, 'wb') as o:
         input = i.read()
+        # Remove the background using rembg
+        print("Removing the background...")
         output = remove(input)
+
         o.write(output)
+        print(f"Image with removed background saved to {output_path}")
